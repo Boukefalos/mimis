435bd019098e7b7bc17805e382228a738d5bf75e
Rik Veenboer <rik.veenboer@gmail.com>
2014-11-29 14:11:52 +0000

    Add code to test native libraries

---
+++ java/test/src/main/test/jni/TestWiiuse.java
@@ -0,0 +1,9 @@
+package jni;
+
+import wiiusej.WiiUseApiManager;
+
+public class TestWiiuse {
+    public static void main(String[] args) {
+    	WiiUseApiManager.getWiimotes(0, false);
+    }
+}
---
+++ java/test/src/main/test/jni/TestXinput.java
@@ -0,0 +1,9 @@
+package jni;
+
+import de.hardcode.jxinput.JXInputManager;
+
+public class TestXinput {
+	public static void main(String[] args) {
+		System.out.println(JXInputManager.getNumberOfDevices());
+	}
+}
