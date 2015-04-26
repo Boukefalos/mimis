1d23467f11e1c68794170d60172d098283962d47
Rik Veenboer <rik.veenboer@gmail.com>
2015-04-26 11:32:52 +0100

    Add various utility classes

---
+++ java/base/src/main/java/base/util/Bufferable.java
@@ -0,0 +1,6 @@
+package base.util;
+
+public interface Bufferable {
+	public void load();
+	public void unload();
+}
---
+++ java/base/src/main/java/base/util/Dummy.java
@@ -0,0 +1,18 @@
+package base.util;
+
+public class Dummy implements Bufferable {
+	public int id;
+	
+	public Dummy(int id) {
+		this.id = id;
+	}
+
+	public void load() {
+		System.out.println("Dummy #" + id + ": load()");		
+	}
+
+	public void unload() {
+		System.out.println("Dummy #" + id + ": load()");		
+	}
+
+}
