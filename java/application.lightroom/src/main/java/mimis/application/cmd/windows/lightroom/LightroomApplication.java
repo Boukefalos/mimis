ea705316f009407f97eb9d3b810860fcd59f6816
Rik Veenboer <rik.veenboer@gmail.com>
2016-07-11 23:21:06 +0100

    Add/update license header in source files

--- java/application.lightroom/src/main/java/mimis/application/cmd/windows/lightroom/LightroomApplication.java
+++ java/application.lightroom/src/main/java/mimis/application/cmd/windows/lightroom/LightroomApplication.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 Rik Veenboer <rik.veenboer@gmail.com>
+ * Copyright (C) 2016 Rik Veenboer <rik.veenboer@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package mimis.application.cmd.windows.lightroom;
 
 import base.exception.worker.ActivateException;
--- java/exec.mimis/src/main/java/mimis/value/Target.java
+++ java/exec.mimis/src/main/java/mimis/value/Target.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 Rik Veenboer <rik.veenboer@gmail.com>
+ * Copyright (C) 2016 Rik Veenboer <rik.veenboer@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
